a3586234802af7a226584dcbf6e04d3d8d45922c
Thomas Kairu <tmuchomba@gmail.com>
2024-12-03 23:38:49 +0300

    Match Render's example code exactly

--- ErickMotors/server.js
+++ ErickMotors/server.js
@@ -1,12 +1,12 @@
-import express from 'express';
-const app = express();
+const express = require('express')
+const app = express()
 
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello from ErickMotors!');
-});
+  res.send('Hello World!')
+})
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+  console.log('Server is running on port', PORT)
+})
